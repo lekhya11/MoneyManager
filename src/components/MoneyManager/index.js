b191b8fa33cf79a28f68a3a21dbcab2ed4f1ff00
k.lekhya <107404763+lekhya11@users.noreply.github.com>
2022-09-23 15:07:06 +0530

    Update index.js

--- src/components/MoneyManager/index.js
+++ src/components/MoneyManager/index.js
@@ -4,12 +4,31 @@ import MoneyDetails from '../MoneyDetails'
 const MoneyManager = () => (
   <div className="money-container">
     <div className="person-details">
-      <h1 className="heading">HI, Richard</h1>
+      <h1 className="main-heading">HI, Richard</h1>
       <p className="paragraph">
         Welcome back to you <span className="highLight"> Money Manager</span>
       </p>
     </div>
     <MoneyDetails />
+    <div className="bottom-container">
+      <div className="form-container">
+        <h1 className="heading">Add Transaction</h1>
+        <form>
+          <label htmlFor="title">TITLE</label>
+          <input type="text" id="title" className="input" />
+          <label htmlFor="amount">AMOUNT</label>
+          <input type="text" id="amount" className="input" />
+          <label htmlFor="select">TYPE</label>
+          <select id="select">
+            <option>Income</option>
+            <option>Expenses</option>
+          </select>
+        </form>
+      </div>
+      <div className="history-container">
+        <h1 className="heading">History</h1>
+      </div>
+    </div>
   </div>
 )
 
